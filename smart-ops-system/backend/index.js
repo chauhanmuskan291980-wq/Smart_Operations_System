5a73802f8499b6a272783d755bcdc27a8339f3df
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-04-08 10:29:43 +0530

    update server port from 5000 to 7000

--- smart-ops-system/backend/index.js
+++ smart-ops-system/backend/index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks',taskRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`🚀 Server flying on port ${PORT}`));
